0917ab55b1c40137267e8736c67f0784b43cfb00
Bounnoy Phanthavong <bounnoy@bounnoy.com>
2020-02-06 22:06:58 -0800

    initial code

---
+++ nn.py
@@ -0,0 +1,45 @@
+# Bounnoy Phanthavong (ID: 973081923)
+# Homework 2
+#
+# This is a machine learning program that models a neural network.
+# Here, we implement a two-layer neural network with one hidden-layer
+# to perform handwritten digit recognition.
+# This program was built in Python 3.
+
+from pathlib import Path
+import numpy as np
+import csv
+import pickle
+
+if __name__ == '__main__':
+
+    pklTrain = Path("mnist_train.pkl")
+    pklTest = Path("mnist_test.pkl")
+    fileTrain = Path("mnist_train.csv")
+    fileTest = Path("mnist_test.csv")
+
+    if not fileTrain.exists():
+        sys.exit("mnist_train.csv not found")
+
+    if not fileTest.exists():
+        sys.exit("mnist_test.csv not found")
+
+    if not pklTrain.exists():
+        f = np.genfromtxt("mnist_train.csv", delimiter=",")
+        csv = open("mnist_train.pkl", 'wb')
+        pickle.dump(f, csv)
+        csv.close()
+
+    if not pklTest.exists():
+        f = np.genfromtxt("mnist_test.csv", delimiter=",")
+        csv = open("mnist_test.pkl", 'wb')
+        pickle.dump(f, csv)
+        csv.close()
+
+    file = open("mnist_train.pkl", "rb")
+    train = pickle.load(file)
+    file.close()
+
+    file = open("mnist_test.pkl", "rb")
+    test = pickle.load(file)
+    file.close()
